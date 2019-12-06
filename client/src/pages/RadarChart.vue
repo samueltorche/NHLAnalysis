381d6e377ad93bf9d88b0f95dee5738b74ececee
Samuel Torche <samuel.torche@edu.hefr.ch>
2019-12-06 17:04:15 +0100

    ..

---
+++ client/src/pages/RadarChart.vue
@@ -0,0 +1,16 @@
+<script>
+  import {Radar} from 'vue-chartjs'
+
+  export default {
+    name: "RadarChart",
+    extends: Radar,
+    props: ['chartdata', 'options'],
+    mounted() {
+      this.renderChart(this.chartdata, this.options)
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
